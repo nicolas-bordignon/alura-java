aa25c3b9075560574262c3e11452f307b2da4bd3
nicolas-bordignon <nicolas.bordignon@gmail.com>
2025-02-11 20:58:22 -0300

    Aula 1 -  Primeiras classes

---
+++ src/Estudos.java
@@ -0,0 +1,9 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("""
+                Anotações de Estudo:
+                JVM - Máquina virtual Java
+                JRE - Java Runtime Environment
+                """);
+    }
+}
